66b2574ac1b89c587a4a68891c210e26afc07e05
kreuzerk <kevin.kreuzer90@icloud.com>
2021-09-05 20:40:58 +0200

    feat(options): add silent option

--- src/replace-json-property.js
+++ src/replace-json-property.js
@@ -3,20 +3,24 @@ const log = require('./log');
 
 const DEFAULT_OPTIONS = {
     spaces: 2,
-    EOL: '\n'
+    EOL: '\n',
+    SILENT: false
 };
 
 const replace = (path, property, newValue, options) => {
     options = {
         spaces: (options && options.spaces) || DEFAULT_OPTIONS.spaces,
-        EOL: (options && options.EOL) || DEFAULT_OPTIONS.EOL
+        EOL: (options && options.EOL) || DEFAULT_OPTIONS.EOL,
+        silent: (options && options.silent) || DEFAULT_OPTIONS.SILENT
     };
     try {
         const file = readFile(path, property, newValue);
         jsonfile.writeFileSync(path, file, options);
-        log.success(
-            `Property: "${property}" in file: ${path} successfully overwritten with "${newValue}"`
-        );
+        if (!options.silent) {
+            log.success(
+                `Property: "${property}" in file: ${path} successfully overwritten with "${newValue}"`
+            );
+        }
     } catch (error) {
         log.error(error);
     }
